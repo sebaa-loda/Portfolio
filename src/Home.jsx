893be9e26af46f68bdfbb8a9a33482b8908b5fad
Sebaa <sebaamdq96@gmail.com>
2024-03-18 17:19:48 -0300

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,23 @@
-
+import {
+  About,
+  Welcome,
+  Skills,
+  Contact,
+  Proyects,
+  Education,
+} from "./pages";
 
 export const Home = () => {
   return (
     <div>
       <h1>Bienvenidos!</h1>
 
+      <Welcome />
+      <About />
+      <Proyects/>
+      <Education/>
+      <Skills/>
+      <Contact/>
     </div>
-  )
-}
+  );
+};
---
+++ src/pages/Proyects.jsx
@@ -0,0 +1,7 @@
+
+
+export const Proyects = () => {
+  return (
+    <div>Proyects</div>
+  )
+}
